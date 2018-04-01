fb72f2380c2a14dd1eb603f7f49ff77e6a5501f1
judaikawa <32301836+judaikawa@users.noreply.github.com>
2018-04-01 14:31:44 -0300

    Add files via upload

---
+++ Contents.swift
@@ -0,0 +1,31 @@
+//: # Scape the iPhone
+//: A swift playground by Juliana Daikawa
+/*:
+ 
+ Hi there! Something really odd has happened. Poor Aria got somehow stuck inside someone else's iPhone, and she is trying to get out.
+ 
+ Help her out, by going through the apps. To do so, use the direction buttons  ![descricao](arrow-buttons.png "") (you can also long press if you are in a hurry!) to walk around and press the A button ![descricao](a-button.png "") while on top of the app you want to take a look.
+ 
+ While inside an app, you can check it's features (you can go ahead and tap the screen to explore the app, like a normal iPhone) and once done, press the B button ![descricao](b-button.png "") to return to the main screen and explore other apps, to try to figure out who this phone belongs to and how to get out.
+ 
+ **Hint:** Check every app at least once
+ 
+ 
+ You can also press the start button ![descricao](start-button.png "") for help and credits, while on the main screen.
+ */
+/*:
+ - > This playground was made with a fixed frame of 450x600, tested on a MacBook Pro 13.3"
+ */
+import UIKit
+import PlaygroundSupport
+
+// Present the view controller in the Live View window
+masterViewController.preferredContentSize = CGSize(width: 450, height: 600)
+PlaygroundPage.current.liveView = masterViewController
+
+/*:
+ Credits:
+ Developed and designed by Juliana Daikawa;
+ Character pixel art by GugaJokes;
+ Musics "8 Bit Beggining!", "8 Bit Fun!", "8 Bit Street!" and "Victory Song!" by HeatleyBros
+*/
